aa358375e752ec22fc84fd61b4da36b5074c5d99
Raphael Lullis <raphael@mushroomlabs.com>
2025-12-13 20:14:52 +0100

    add command to resolve reference

---
+++ activitypub/management/commands/resolve_reference.py
@@ -0,0 +1,21 @@
+import logging
+
+from activitypub.models import Reference
+from django.core.management.base import BaseCommand
+
+logger = logging.getLogger(__name__)
+
+
+class Command(BaseCommand):
+    help = "Manually resolve references"
+
+    def add_arguments(self, parser):
+        parser.add_argument("uris", type=str, nargs="*", help="Reference URIs")
+
+    def handle(self, *args, **options):
+        uris = options["uris"]
+
+        for uri in uris:
+            logger.info(f"Resolving: {uri}")
+            reference = Reference.make(uri)
+            reference.resolve()
